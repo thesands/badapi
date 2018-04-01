f7f5512ec030530c54ed0964eff7e521ae6449c5
Dana Sanders <danasanders@expedia.com>
2018-04-01 00:05:52 -0700

    updating readme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,108 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function getHeaders(data) {
+   let headers = [], header;
+
+   for (let d in data) {
+      header = '';
+
+      if (d === 'id') continue;
+
+      let str = d.split(/(?=[A-Z])/);
+      str[0] = str[0].charAt(0).toUpperCase() + str[0].slice(1);
+
+      for (let s = 0; s < str.length; s++) {
+         header += str[s] + ' ';
+      }
+
+      headers.push({title: header.substring(0, header.length-1), sort: 'asc'});
+   }
+
+   return headers;
 }
 
-export default App;
+function Table(props) {
+   const td = props.tableData;
+   const headers = td.headers.map((header, index) => <th key={index} id={header.title} className={td.headerClass}>{header.title}</th>);
+   const data = td.rows.map((row, index) => <TableRow key={index} data={row} />);
+
+   return (
+      <table id={props.id} className={props.className}>
+         <thead>
+         <tr>
+            {headers}
+         </tr>
+         </thead>
+         <tbody>
+         {data}
+         </tbody>
+      </table>
+   );
+}
+
+function TableRow(props) {
+   const data = props.data;
+   let row = [];
+
+   for (let d in data) {
+      if (d !== 'id') {
+         row.push(<td key={row.length}>{data[d]}</td>);
+      }
+   }
+
+   return (
+      <tr>{row}</tr>
+   );
+}
+
+class BadApi extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         error: null,
+         isLoaded: false,
+         table: {
+            headers: [],
+            rows: [],
+         },
+      };
+   }
+
+   componentDidMount() {
+      fetch("http://localhost:8080/api/values")
+         .then(res => res.json())
+         .then(
+            (result) => {
+               this.setState({
+                  isLoaded: true,
+                  table: {headers: getHeaders(result[0]), rows: result},
+                  tweets: result
+               });
+            },
+            (error) => {
+               this.setState({
+                  isLoaded: true,
+                  error
+               });
+            }
+         )
+   }
+
+   render() {
+      const { error, isLoaded } = this.state;
+      if (error) {
+         return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+         return <div>Loading...</div>;
+      } else {
+         return (
+            <div className="App">
+               <Table id={'tweetTable'} className={'tweetTable'} tableData={this.state.table} />
+            </div>
+         );
+      }
+   }
+}
+
+export default BadApi;
